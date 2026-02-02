25c037a57c4d1e4e85b2a6801c31d482a59a009a
zeekhoft <dltposiedon@gmail.com>
2026-02-02 09:40:20 +0800

    adding a simple CLI

--- main.py
+++ main.py
@@ -1,19 +1,38 @@
 
-from encryption import Encrypt
-with open("test.txt") as f:
-    words = (f.readline())
-special_key = "potato"
-secret_word = words
+import cmd
 
 
-sol = Encrypt(special_key, secret_word)
-sol.generate_special_key()
-(sol.encrypt_to_numbers()) #turn the ascii numbers and add more complexity to them
-(sol.encryption_conversion()) #convert the list to string then to int then divide
-print(sol.final_encryption()) #need output
-print(sol.breakdown())
+# from encryption import Encrypt
+# with open("test.txt") as f:
+#     words = (f.readline())
+# special_key = "potato"
+# secret_word = words
 
-(sol.decryption_of_list())
-(sol.list_number_decryption(sol.decryption_of_list()))
-print(sol.final_decryption()) #need output
 
+# sol = Encrypt(special_key, secret_word)
+# sol.generate_special_key()
+# (sol.encrypt_to_numbers()) #turn the ascii numbers and add more complexity to them
+# (sol.encryption_conversion()) #convert the list to string then to int then divide
+# print(sol.final_encryption()) #need output
+# print(sol.breakdown())
+
+# (sol.decryption_of_list())
+# (sol.list_number_decryption(sol.decryption_of_list()))
+# print(sol.final_decryption()) #need output
+
+class MyCLI(cmd.Cmd):
+    prompt = '[;-)]:'
+    intro = 'Welcome to MyCLI. Type "help" for available commands.'
+
+    def do_hello(self, line):
+        """Print a greeting."""
+        print("Hello, World!")
+
+    def do_quit(self, line):
+        """Exit the CLI."""
+        return True
+    
+
+
+if __name__ == '__main__':
+    MyCLI().cmdloop()
